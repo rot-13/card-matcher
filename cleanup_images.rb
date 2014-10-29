1569d5b40b9bd1edb26b28a7801a9a3061e6da97
Itay Adler <itayadler@gmail.com>
2014-10-29 16:50:25 +0200

    Fix cleanup_images

--- cleanup_images.rb
+++ cleanup_images.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/ruby
 
 imagesDbs = Dir.glob('./clusters/**/*.db').each { |imagesDb| File.unlink(imagesDb) }
-File.unlink('./templates.db')
+File.unlink('./templates.db') if File.exist? './templates.db'
